341a4106d2f63a275236f563e8448dd50a99715e
Karl Heinz Marbaise <khmarbaise@apache.org>
2016-01-15 16:17:22 +0100

    Removed commons-collections4 cause it does not contain what i need.

--- src/test/java/com/soebes/maven/extensions/FirstTest.java
+++ src/test/java/com/soebes/maven/extensions/FirstTest.java
@@ -4,11 +4,21 @@ import org.testng.annotations.Test;
 
 public class FirstTest
 {
-
+ 
+    public class X {
+        public String get (String project, String phase, String mojo, String goal) {
+            return null;
+        }
+    }
+    /*
+     * Project : phase : mojo : goal
+     */
     @Test
-    public void firstTest() {
-//        MultiKeyMap.multiKeyMap(new LinkedMap< new MultiKey("A", "B"), String>());
-//
-//        System.out.println( "Test" );
+    public void firstTest()
+    {
+        // definition however..
+        X result = new X();
+        String value = result.get("p", "clean", "mojo", "exec");
+        
     }
 }
