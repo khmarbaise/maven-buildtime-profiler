9f9abcbce390cc5e6330ea91d97978edfa9fbfaf
Karl Heinz Marbaise <kama@soebes.de>
2025-04-13 09:32:26 +0200

    Removed SVN from template.

--- src/main/java-templates/com/soebes/maven/extensions/BuildTimeProfilerVersion.java
+++ src/main/java-templates/com/soebes/maven/extensions/BuildTimeProfilerVersion.java
@@ -4,7 +4,6 @@ public final class BuildTimeProfilerVersion {
 
     private static final String VERSION = "${project.version}";
     private static final String GROUPID = "${project.groupId}";
-    private static final String SVN = "${project.scm.developerConnection}";
     private static final String SVN_BRANCH = "${scmBranch}";
     private static final String REVISION = "${buildNumber}";
 
@@ -13,23 +12,19 @@ public final class BuildTimeProfilerVersion {
         // no one should create an instance of this class.
     }
 
-    public static final String getVersion() {
+    public static String getVersion() {
         return VERSION;
     }
 
-    public static final String getGroupId() {
+    public static String getGroupId() {
         return GROUPID;
     }
 
-    public static final String getSVN() {
-        return SVN;
-    }
-
-    public static final String getRevision() {
+    public static String getRevision() {
         return REVISION;
     }
 
-    public static final String getSVNBranch() {
+    public static String getSVNBranch() {
         return SVN_BRANCH;
     }
 }
